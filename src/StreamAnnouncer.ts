7b35558660d7773cebf57081c1b32e5ee3cf7b97
MatthewSH <MatthewSH@users.noreply.github.com>
2018-08-08 23:53:56 -0400

    More base code.

---
+++ src/StreamAnnouncer.ts
@@ -0,0 +1,59 @@
+import * as Discord from "discord.js";
+import { Database } from "./lib/Database";
+
+let discord = new Discord.Client();
+let database = new Database();
+let commandRoleId = database.database().get("config.discord.commandRoleId").value();
+let announcementChannelId = database.database().get("config.discord.announcmentChannelId").value();
+
+discord.on("ready", () => {
+    console.log("Discord client ready.");
+    console.log(`Allowing users with the role id ${commandRoleId} to add streamers`);
+});
+
+discord.on("error", (error) => {
+    console.error(error);
+});
+
+discord.on("message", (message: any) => {
+    if (message.member.roles.has(commandRoleId)) {
+        if (message.content.indexOf("!addstreamer") > -1) {
+            let parameters = message.content.split(" ").slice(1);
+
+            parameters = parameters.filter((element: any) => {
+                return (element.length > 0 && element !== undefined && element !== null);
+            });
+
+
+            if (parameters.length < 1) {
+                message.delete();
+                message.reply(`${message.author.username}, you must include a StreamMe username.`)
+                       .then((msg: any) => {
+                           msg.delete(6000);
+                       });
+
+                return;
+            }
+
+            let username = parameters[0];
+
+            message.delete();
+
+            if (database.users().add(username)) {
+                message.reply(`${message.author.username}, the streamer ${username} has been added.`)
+                       .then((msg: any) => {
+                           msg.delete(6000);
+                       });
+            } else {
+                message.reply(`${message.author.username}, something went wrong. Is ${username} a valid streamer name?`)
+                       .then((msg: any) => {
+                           msg.delete(6000);
+                       });
+            }
+        }
+    }
+});
+
+let token = database.database().get("config.discord.botToken").value();
+
+discord.login(token);
---
+++ src/lib/Database.ts
@@ -0,0 +1,64 @@
+import * as lowdb from "lowdb";
+import * as FileSync from "lowdb/adapters/FileSync";
+import * as request from "request-promise";
+
+export class Database {
+    private db: lowdb.LowdbSync<any>;
+    private usersModel: Users;
+
+    constructor() {
+        this.db = lowdb(new FileSync("stream-announcer.json"));
+
+        this.db.defaults({
+            "config": {
+                "discord": {
+                    "announcmentChannelId": "1",
+                    "commandRoleId": "1",
+                    "botToken": "tokenhere",
+                },
+                "checkTime": 15000,
+                "users": []
+            },
+            "history": []
+        }).write();
+
+        this.usersModel = new Users(this.db);
+    }
+
+    public database(): lowdb.LowdbSync<any> {
+        return this.db;
+    }
+
+    public users(): Users {
+        return this.usersModel;
+    }
+}
+
+class Users {
+    constructor(private db: lowdb.LowdbSync<any>) {}
+
+    public all(): Array<string> {
+        return this.db.get("config.users").value();
+    }
+
+    public add(username: string): boolean {
+        request({
+            method: "GET",
+            uri: `https://www.stream.me/api-user/v1/${username}/channel`,
+            json: true
+        }).then(response => {
+            let userId = response.userPublicId;
+
+            if (!this.db.get("config.users").value().includes(userId)) {
+                this.db.get("config.users").push(userId).write();
+            }
+
+            return true;
+        }).catch(error => {
+            console.log(error);
+            return false;
+        });
+
+        return true;
+    }
+}
